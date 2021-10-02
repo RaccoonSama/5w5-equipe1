8374c7b80cf0cadcd7f845100159f116af2e8ea4
Younes4260 <70180315+Younes4260@users.noreply.github.com>
2021-10-02 00:19:44 -0400

    Implémentation de la page 404 et création du design de base

--- troisdaccord/footer.php
+++ troisdaccord/footer.php
@@ -16,13 +16,12 @@
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'troisdaccord' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'troisdaccord' ), 'WordPress' );
+				//printf( esc_html__( 'Proudly powered by %s', 'troisdaccord' ), 'WordPress' );
 				?>
 			</a>
-			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'troisdaccord' ), 'troisdaccord', '<a href="http://underscores.me/">les 3D daccord</a>' );
+				//printf( esc_html__( 'Theme: %1$s by %2$s.', 'troisdaccord' ), 'troisdaccord', '<a href="http://underscores.me/">les 3D daccord</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
--- troisdaccord/header.php
+++ troisdaccord/header.php
@@ -31,17 +31,17 @@
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			
 				<?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			
 				<?php
 			endif;
-			$troisdaccord_description = get_bloginfo( 'description', 'display' );
+			
 			if ( $troisdaccord_description || is_customize_preview() ) :
 				?>
-				<p class="site-description"><?php echo $troisdaccord_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+				<p class="site-description"><?php //echo $troisdaccord_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
