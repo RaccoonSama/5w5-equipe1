cf8b21f2410172e1523fa83c8612bc9249330bb6
Younes4260 <70180315+Younes4260@users.noreply.github.com>
2021-12-05 19:58:51 -0500

    version mobile/tablette projets

--- troisdaccord/js/projets.js
+++ troisdaccord/js/projets.js
@@ -9,21 +9,30 @@ scrollContainer2.addEventListener("wheel", (evt) => {
     if(nb2 >= 0) {
         nb2 = 0;
     }
-    if(nb2 <= -1000) {
-        nb2 = -1000;
+    if(nb2 <= -2000) {
+        nb2 = -2000;
     }
     evt.preventDefault();
-    scrollContainer2.style.transform = "translateX(" + nb2/5 + "vw)";
-    scrollPoint.style.width = "" + -1*(nb2/10) + "vw";
+    scrollContainer2.style.transform = "translateX(" + nb2/4.5 + "vw)";
+    scrollPoint.style.width = "" + -1*(nb2/20) + "vw";
 
 });
 
 const MyNodeList = document.querySelectorAll('.proj-content');
 
-document.addEventListener('mousemove', function (e) {
-    for (let i = 0; i < MyNodeList.length; i++) {
-        var xAxis = (window.innerWidth / 2 - e.pageX) / -30;
-        var yAxis = (window.innerHeight / 2 - e.pageY) / 15;
-        MyNodeList[i].style.transform = 'rotateY(' + xAxis + 'deg) rotateX(' + yAxis + 'deg)';
-      }
-});
+/* window.addEventListener('resize', function(){
+    if(window.innerWidth < 1000){
+        document.removeEventListener('mousemove');
+    }
+    if(window.innerWidth > 1000){
+        document.EventListener('mousemove', function (e) {
+            for (let i = 0; i < MyNodeList.length; i++) {
+                var xAxis = (window.innerWidth / 2 - e.pageX) / -30;
+                var yAxis = (window.innerHeight / 2 - e.pageY) / 15;
+                MyNodeList[i].style.transform = 'rotateY(' + xAxis + 'deg) rotateX(' + yAxis + 'deg)';
+              }
+        });
+    }
+}); */
+
+
